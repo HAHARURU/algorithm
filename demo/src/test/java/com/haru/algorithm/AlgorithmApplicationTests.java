676272033cafcab0ae5f9d12e3d249bee1638ca4
liujiangchun <liujiangchun@changingedu.com>
2021-08-24 09:34:51 +0800

    同步工作时的代码

--- demo/src/test/java/com/haru/algorithm/AlgorithmApplicationTests.java
+++ demo/src/test/java/com/haru/algorithm/AlgorithmApplicationTests.java
@@ -22,6 +22,21 @@ class AlgorithmApplicationTests {
     /**
      * ---------------------------- 时间复杂度 ----------------------------
      */
+    int n;
+    int[] arrays = new int[n];
+    int count = 0;
+
+    public void insert(int value) {
+        if (count == arrays.length) {
+            int sum = 0;
+            for (int array : arrays) {
+                sum += array;
+            }
+            arrays[0] = sum;
+            count = 1;
+        }
+        arrays[count++] = value;
+    }
 
     /**
      * ---------------------------- 数组 ----------------------------
@@ -43,8 +58,80 @@ class AlgorithmApplicationTests {
     }
 
     /**
+     * 单向链表判断回文字符串
+     *
+     * @param listNode
+     * @return
+     */
+    public Boolean isPalindrome(LinkNode listNode) {
+        if (listNode == null || listNode.next == null) {
+            return false;
+        }
+
+        LinkNode slow = listNode;
+        LinkNode fast = listNode;
+        LinkNode reverse = null;
+
+        // 链表长度可能是偶数，可能是奇数；偶数fast会为null
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            // 单向链表反转
+            LinkNode next = slow.next;
+            slow.next = reverse;
+            reverse = slow;
+            slow = next;
+        }
+
+        // 链表长度为奇数，slow要向前进一个，抛弃最中间的
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        while (slow != null) {
+            assert reverse != null;
+            if (!slow.value.equals(reverse.value)) {
+                return false;
+            }
+            slow = slow.next;
+            reverse = reverse.next;
+        }
+
+        return true;
+    }
+
+    /**
      * ---------------------------- 栈 ----------------------------
      */
+
+    /**
+     * 数组实现栈
+     */
+    class Stack {
+        private String[] items;
+        private int size;
+        private int count;
+
+        public Stack(int size) {
+            this.items = new String[size];
+            this.size = size;
+            this.count = 0;
+        }
+
+        public Boolean push(String value) {
+            if (count == size) {
+                return false;
+            }
+            items[count++] = value;
+            return true;
+        }
+
+        public String pop() {
+            if (count == 0) {
+                return null;
+            }
+            return items[count-- - 1];
+        }
+    }
 
     /**
      * 链表实现栈
@@ -76,4 +163,5 @@ class AlgorithmApplicationTests {
     /**
      * ---------------------------- 队列 ----------------------------
      */
+
 }
