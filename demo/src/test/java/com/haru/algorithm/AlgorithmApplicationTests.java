883f473e40b9e2fa61baa028c76fd72e16ca93b4
harurunrun <18055394367@163.com>
2021-09-03 22:04:32 +0800

    0-1背包

--- demo/src/test/java/com/haru/algorithm/AlgorithmApplicationTests.java
+++ demo/src/test/java/com/haru/algorithm/AlgorithmApplicationTests.java
@@ -1855,4 +1855,49 @@ class AlgorithmApplicationTests {
         return true;
     }
 
+    /**
+     * ---------------------------- 动态规划 ----------------------------
+     */
+
+
+    public static int knapsack(int[] weight, int[] value, int n, int w) {
+        int[][] states = new int[n][w+1];
+        for (int i = 0; i < n; ++i) { // 初始化states
+            for (int j = 0; j < w+1; ++j) {
+                states[i][j] = -1;
+            }
+        }
+        states[0][0] = 0;
+        if (weight[0] <= w) {
+            states[0][weight[0]] = value[0];
+        }
+        for (int i = 1; i < n; ++i) { //动态规划，状态转移
+            for (int j = 0; j <= w; ++j) { // 不选择第i个物品
+                if (states[i-1][j] >= 0) states[i][j] = states[i-1][j];
+            }
+            for (int j = 0; j <= w-weight[i]; ++j) { // 选择第i个物品
+                if (states[i-1][j] >= 0) {
+                    int v = states[i-1][j] + value[i];
+                    if (v > states[i][j+weight[i]]) {
+                        states[i][j+weight[i]] = v;
+                    }
+                }
+            }
+        }
+        // 找出最大值
+        int maxvalue = -1;
+        for (int j = 0; j <= w; ++j) {
+            if (states[n-1][j] > maxvalue) maxvalue = states[n-1][j];
+        }
+        return maxvalue;
+    }
+
+
+    /*for (int i = n-1; i >= 1; --i) { // i表示二维数组中的行，j表示列
+        if(j-items[i] >= 0 && states[i-1][j-items[i]] == true) {
+            System.out.print(items[i] + " "); // 购买这个商品
+            j = j - items[i];
+        } // else 没有购买这个商品，j不变。
+    }
+        if (j != 0) System.out.print(items[0]);*/
 }
